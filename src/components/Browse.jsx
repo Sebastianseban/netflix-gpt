684619afb87896671eac6af9d5a0054296bfba77
sebastian <sebastianseban0105@gmail.com>
2025-05-20 17:57:01 +0530

    refactor: extract now playing movies fetch logic into custom hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,24 +1,8 @@
-import React, { useEffect } from 'react'
+
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/movieSlice'
+
 
 const Browse = () => {
-
-  const dispatch = useDispatch()
-   
-  const getNowPlaingMovies = async () =>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_OPTIONS)
-    const json = await data.json()
-    console.log(json.results )
-    dispatch(addNowPlayingMovies(json.results))
-  }
-  useEffect(() => {
-  
-  getNowPlaingMovies()
-    
-  }, [])
   
   return (
     <div>
