59b0efa438b3cac7c0f7b82ff8217bede5eff65e
sebastian <sebastianseban0105@gmail.com>
2025-05-27 06:09:39 +0530

    feat:added movielist and trending movie api

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
+
+  const movies = useSelector((store)=>store.movies)
+
+
   return (
-    <div>secondaryContainer</div>
+    movies.nowPlayingMovies &&
+    <div className='bg-black'>
+     <div className='-mt-20 pl-12 relative z-20'>
+       <MovieList title={'Now playing'} movies={movies.nowPlayingMovies}/>
+      <MovieList title={'Trending'} movies={movies.trendingMovies}/>
+      <MovieList title={'Popular'} movies={movies.popularMovies}/>
+      <MovieList title={'upcoming Moives'} movies={movies.nowPlayingMovies}/>
+     
+     </div>
+    </div>
   )
 }
 
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,18 +4,31 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
-    trailerVideo:null
+    trailerVideo: null,
+    popularMovies: null,
+    trendingMovies: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
-    addTrailerVideo: (state,action) => {
-      state.trailerVideo = action.payload
-    }
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTrendingMovies: (state, action) => {
+      state.trendingMovies = action.payload;
+    },
   },
 });
 
-export const {addNowPlayingMovies,addTrailerVideo} = moviesSlice.actions
+export const {
+  addNowPlayingMovies,
+  addTrailerVideo,
+  addPopularMovies,
+  addTrendingMovies,
+} = moviesSlice.actions;
 
-export default moviesSlice.reducer
+export default moviesSlice.reducer;
